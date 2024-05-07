1b8bbb55d33931112578a3bad3675cbea79883cf
Gábor Librecz <kuglee@gmail.com>
2024-05-07 16:19:47 +0200

    Initial Commit

---
+++ SharedTest/SharedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedTestApp.swift
+//  SharedTest
+//
+//  Created by Librecz Gábor on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SharedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
